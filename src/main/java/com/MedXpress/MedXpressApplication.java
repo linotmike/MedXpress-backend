f9c71e21e7f17e346760cb2013235b4bc39d0f3b
linotmike <128604219+linotmike@users.noreply.github.com>
2025-11-27 15:36:23 -0600

    Initial commit

---
+++ src/main/java/com/MedXpress/MedXpressApplication.java
@@ -0,0 +1,13 @@
+package com.MedXpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedXpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedXpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MedXpress/MedXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedXpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
